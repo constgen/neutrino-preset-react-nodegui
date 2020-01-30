d0d7abd140e843bc1e4423f43e3589fc80c3cddd
constgen <const.gen@gmail.com>
2020-01-30 23:46:36 +0200

    Better Babel compilation

--- src/middlewares/babel.js
+++ src/middlewares/babel.js
@@ -2,6 +2,7 @@ let devMode = (process.env.NODE_ENV === 'development');
 let compileLoader = require('@neutrinojs/compile-loader');
 let babelMerge = require('babel-merge');
 let deepmerge = require('deepmerge');
+let { shouldPrintComment } = require('babel-plugin-smart-webpack-import');
 
 module.exports = function (customSettings = {}) {
 	return function (neutrino) {
@@ -18,7 +19,18 @@ module.exports = function (customSettings = {}) {
 				include: [neutrino.options.source, neutrino.options.tests],
 				babel: babelMerge(
 					{
-						plugins: [require.resolve('@babel/plugin-syntax-dynamic-import')],
+						plugins: [
+							require.resolve('@babel/plugin-syntax-dynamic-import'),
+							[require.resolve('@babel/plugin-proposal-decorators'), {
+								decoratorsBeforeExport: true
+							}],
+							require.resolve('@babel/plugin-proposal-class-properties'),
+							[require.resolve('babel-plugin-transform-jsx-url'), {
+								root: neutrino.options.source,
+								attrs: ['img:src', 'link:href', 'Image:src', 'video:src', 'Video:src', 'audio:src']
+							}],
+							require.resolve('babel-plugin-smart-webpack-import')
+						],
 						presets: [
 							[
 								require.resolve('@babel/preset-env'),
@@ -33,7 +45,8 @@ module.exports = function (customSettings = {}) {
 							require.resolve('@babel/preset-react')
 						],
 						cacheDirectory: true, 
-						cacheCompression: false
+						cacheCompression: false,
+						shouldPrintComment
 					},
 					settings.babel
 				)
