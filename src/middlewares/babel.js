a6626644f78cbbf6d58ce4b1867f641ecfb75df2
constgen <const.gen@gmail.com>
2020-02-23 00:10:25 +0200

    Upgrade Babel configuration

--- src/middlewares/babel.js
+++ src/middlewares/babel.js
@@ -5,6 +5,8 @@ let { shouldPrintComment } = require('babel-plugin-smart-webpack-import')
 
 module.exports = function (customSettings = {}) {
 	return function (neutrino) {
+		let devMode = process.env.NODE_ENV === 'development'
+		let prodMode = process.env.NODE_ENV === 'production'
 		let defaultSettings = {
 			babel: {},
 			targets: { node: process.versions.node }
@@ -24,25 +26,43 @@ module.exports = function (customSettings = {}) {
 								decoratorsBeforeExport: true
 							}],
 							require.resolve('@babel/plugin-proposal-class-properties'),
+							[require.resolve('babel-plugin-jsx-pragmatic'), {
+								module: 'react',
+								export: 'createElement',
+								import: 'createElement'
+							}],
 							[require.resolve('babel-plugin-transform-jsx-url'), {
 								root: neutrino.options.source,
 								attrs: ['img:src', 'link:href', 'Image:src', 'video:src', 'Video:src', 'audio:src', 'Audio:src']
 							}],
-							require.resolve('babel-plugin-smart-webpack-import')
-						],
+							require.resolve('babel-plugin-smart-webpack-import'),
+							prodMode && [require.resolve('babel-plugin-transform-react-remove-prop-types'), {
+								removeImport: true,
+								classNameMatchers: ['Component', 'PureComponent']
+							}]
+						].filter(Boolean),
 						presets: [
 							[
 								require.resolve('@babel/preset-env'),
 								{
 									debug: neutrino.options.debug,
 									targets: settings.targets,
+									spec: false,
+									modules: false,
 									useBuiltIns: coreJsVersion ? 'usage' : false,
 									...(coreJsVersion && { corejs: coreJsVersion.major })
 								}
 							],
-							require.resolve('@babel/preset-typescript'),
-							require.resolve('@babel/preset-react')
-						],
+							[require.resolve('@babel/preset-typescript'), {
+								allowNamespaces: true
+							}],
+							[require.resolve('@babel/preset-react'), {
+								development: devMode,
+								pragma: 'createElement',
+								useSpread: true,
+								useBuiltIns: false
+							}]
+						].filter(Boolean),
 						cacheDirectory: true,
 						cacheCompression: false,
 						shouldPrintComment
